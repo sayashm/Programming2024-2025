e74d9b627014f5204d52da0091e0f4750b3a1164
s.ayashm <s.ayashm@gmail.com>
2024-10-22 20:07:20 +0200

    Week 4: Word evolutions SOLVED.

--- Reeks_04/woordevoluties.py
+++ Reeks_04/woordevoluties.py
@@ -1,2 +1,30 @@
 # https://dodona.be/nl/courses/4157/series/46290/activities/1713291174
 
+word = input('Word: ')
+letter = input('Letter: ')
+
+
+start_letter = word[0]
+end_letter = letter
+
+distance = (ord(end_letter) - ord(start_letter)) % 26
+
+for char in word:
+    start_pos = ord(char) - ord('A')
+    end_pos = (start_pos + distance) % 26
+    corresponding_letter = chr(end_pos + ord('A'))
+
+    print(char, end="")
+
+    i = start_pos + 1
+    while i != end_pos:
+        print(chr((i % 26) + ord('a')), end="")
+        i = (i + 1) % 26
+
+    print(corresponding_letter)
+
+
+
+
+
+
